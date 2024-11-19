8e629580842ba7f7d75d8956712f5deba70f5e5e
Manel Daza (ANEK) <127338918+ManelDaza@users.noreply.github.com>
2024-11-19 23:58:27 +0100

    df

--- src/playerClasses/Pyromantic.java
+++ src/playerClasses/Pyromantic.java
@@ -12,11 +12,11 @@ public class Pyromantic {
     /* 
         GAME RULES:
         Each floor that the pyro survives (floor not room), it goes crazyer (crazy? i was crazy once)
-        The thing about the pyro is that is easeley the most powerfull of all the wizards
+        The thing about the pyro is that is easeley the most powerfull of all the wizards but hte crazyer it gets the most dificult it is to control him
     
         Player actions:
-            1. A weak atack made for quick slashes
-            2. Heal you character a bit
+            1. PyroFire it is a strong atack that increases in power every time you use the crazy one, but it also has a higher probablity of doing the weak atack (50% power)
+            2. Makes your character crazyer (+1.25 attak and takes -75% damage next turn)
             3. Empower your invocations (first empower is 2x hp, second is 2x damage, third is 2x magic)
             4. Invoce a undead using souls
     */
